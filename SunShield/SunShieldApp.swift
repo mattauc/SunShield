5900122acd83215c25a5fe36d8d5fa1f6a6b0485
Matthew Auciello <matt.auciello1999@gmail.com>
2024-06-19 10:31:55 +1000

    Initial Commit

---
+++ SunShield/SunShieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunShieldApp.swift
+//  SunShield
+//
+//  Created by Matthew Auciello on 19/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct SunShieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
